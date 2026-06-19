7dcba56209a3e274fd30cc8410e4257302fceeb3
zannat23 <zannatfeb23@gmail.com>
2026-06-19 12:13:08 +0530

    Time: 0 ms (100%), Space: 13 MB (30.55%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return l
